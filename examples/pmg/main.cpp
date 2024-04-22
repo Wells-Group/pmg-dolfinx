d65d49b6bdbbb94837393f8eec9b61dc2a9877f6
Chris <cnr12@cam.ac.uk>
2024-04-22 14:25:26 +0100

    Add an interpolation kernel

--- examples/pmg/main.cpp
+++ examples/pmg/main.cpp
@@ -1,6 +1,7 @@
 #include "../../src/cg.hpp"
 #include "../../src/chebyshev.hpp"
 #include "../../src/csr.hpp"
+#include "../../src/interpolate.hpp"
 #include "../../src/pmg.hpp"
 #include "../../src/vector.hpp"
 #include "poisson.h"
